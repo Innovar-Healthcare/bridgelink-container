6651e8923833830d96fd9503059b30f007f21f18
Jim Weng <zweng@PCs-MacBook-Pro.local>
2025-04-08 10:05:04 -0500

    add CUSTOM_PROPERTIES,CUSTOM_OPTTIONS,plugin download,custom keystore and keystore credential

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,9 +4,11 @@
 PROPERTIES_FILE="/opt/bridgelink/conf/mirth.properties"
 VMOPTIONS_FILE="/opt/bridgelink/blserver.vmoptions"
 SERVER_ID_FILE="/opt/bridgelink/appdata/server.id"
+KEYSTORE_FILE="/opt/bridgelink/appdata/keystore.jks"
 EXTENSIONS_DIR="/opt/bridgelink/extensions"
 CUSTOM_JARS_DIR="/opt/bridgelink/custom-jars"
 S3_CUSTOM_JARS_DIR="/opt/bridgelink/S3_custom-jars"
+APPDATA_DIR="/opt/bridgelink/appdata"
 
 # Function to update a property in the file
 update_property() {
@@ -42,30 +44,55 @@ if [ ! -z "$SERVER_ID" ]; then
   echo -e "server.id = ${SERVER_ID//\//\\/}" > "$SERVER_ID_FILE"
 fi
 
-# #Create pg user ,database, schema
-# python3 /opt/scripts/dbScript.py
 
-# #AWS Marketplace usage registration
-# # python3 /opt/scripts/registerUsage.py registerUsage
+# Check if CUSTOM_VMOPTIONS environment variable is set and not empty
+if [[ -n "$CUSTOM_VMOPTIONS" ]]; then
+    echo "Downloading custom vmoptions from: $CUSTOM_VMOPTIONS"
 
-# #Get information from secret manager
-# python3 /opt/scripts/updatePropertiesFromSecretManager.py
+    # Create directory if it doesn't exist
+    mkdir -p "$(dirname "$VMOPTIONS_FILE")"
 
+    CURL_OPTS="-sSLf"
+    [ "${ALLOW_INSECURE}" = "true" ] && CURL_OPTS="-ksSLf"
 
+    # Download and overwrite the target file
+    curl --silent --show-error ${CURL_OPTS} "$CUSTOM_VMOPTIONS" -o "$VMOPTIONS_FILE"
 
+    # Check if download succeeded
+    if [[ $? -eq 0 ]]; then
+        echo "Successfully downloaded and saved to $VMOPTIONS_FILE"
+    else
+        echo "Failed to download the vmoptions from $CUSTOM_VMOPTIONS"
+        exit 1
+    fi
+else
+    echo "CUSTOM_VMOPTIONS is not set. Skipping vmoptions download."
+fi
 
-# Calculate 75% of total ECS task def
-# xmx_value_mb=$(($TOTAL_MEMORY * 80 / 100))
-# rounded_xmx_value_mb=$((xmx_value_mb / 1024 * 1024))
-# sed -i "s/-Xmx[0-9]*m/-Xmx${rounded_xmx_value_mb}m/" $VMOPTIONS_FILE
+# Check if CUSTOM_PROPERTIES environment variable is set and not empty
+if [[ -n "$CUSTOM_PROPERTIES" ]]; then
+    echo "Downloading custom mirth.properties from: $CUSTOM_PROPERTIES"
 
+    # Create target directory if it doesn't exist
+    mkdir -p "$(dirname "$PROPERTIES_FILE")"
 
-# # Use aws cli command to download custom mcserver.vmoption to container
-# if [ -n "${S3_VPMOPTIONS_URL}" ]; then
-#   echo "Download vmoptions file from url... ${S3_VPMOPTIONS_URL}"
+    CURL_OPTS="-sSLf"
+    [ "${ALLOW_INSECURE}" = "true" ] && CURL_OPTS="-ksSLf"
 
-#   aws s3 cp ${S3_VPMOPTIONS_URL} ${VMOPTIONS_FILE}
-# fi
+    # Download the file and overwrite the target
+    curl --silent --show-error ${CURL_OPTS} "$CUSTOM_PROPERTIES" -o "$PROPERTIES_FILE"
+
+    # Verify download success
+    if [[ $? -eq 0 ]]; then
+        echo "Successfully downloaded and saved to $PROPERTIES_FILE"
+    else
+        echo "Failed to download from $CUSTOM_PROPERTIES"
+        exit 1
+    fi
+else
+    echo "CUSTOM_PROPERTIES is not set. Skipping mirth.properties download."
+fi
+
 
 # Loop over environment variables with prefix MP_
 for var in $(env | grep '^MP_' | sed 's/=.*//'); do
@@ -87,27 +114,27 @@ for var in $(env | grep '^MP_' | sed 's/=.*//'); do
 done
 
 # # Download and extract extensions if EXTENSIONS_DOWNLOAD is set
-# if [ -n "${EXTENSIONS_DOWNLOAD}" ]; then
-#   echo "Downloading extensions from ${EXTENSIONS_DOWNLOAD}"
-#   cd ${EXTENSIONS_DIR}
+if [ -n "${EXTENSIONS_DOWNLOAD}" ]; then
+  echo "Downloading extensions from ${EXTENSIONS_DOWNLOAD}"
+  cd ${EXTENSIONS_DIR}
 
-#   CURL_OPTS="-sSLf"
-#   [ "${ALLOW_INSECURE}" = "true" ] && CURL_OPTS="-ksSLf"
+  CURL_OPTS="-sSLf"
+  [ "${ALLOW_INSECURE}" = "true" ] && CURL_OPTS="-ksSLf"
 
-#   # Split URLs by space and iterate over them
-#   IFS=',' read -r -a urls <<< "${EXTENSIONS_DOWNLOAD}"
-#   for url in "${urls[@]}"; do
-#     echo "Downloading from ${url}"
-#     # Extract filename from URL
-#     filename=$(basename "$url")
-#     curl ${CURL_OPTS} "${url}" -o "$filename" || { echo "Problem with extensions download from ${url}"; continue; }
+  # Split URLs by space and iterate over them
+  IFS=',' read -r -a urls <<< "${EXTENSIONS_DOWNLOAD}"
+  for url in "${urls[@]}"; do
+    echo "Downloading from ${url}"
+    # Extract filename from URL
+    filename=$(basename "$url")
+    curl ${CURL_OPTS} "${url}" -o "$filename" || { echo "Problem with extensions download from ${url}"; continue; }
 
-#     echo "Extracting contents of $filename"
+    echo "Extracting contents of $filename"
 
-#     jar xf "$filename" || { echo "Problem extracting contents of $filename"; continue; }
-#     rm "$filename"
-#   done
-# fi
+    jar xf "$filename" || { echo "Problem extracting contents of $filename"; continue; }
+    rm "$filename"
+  done
+fi
 
 # Download and extract jars if CUSTOM_JARS_DOWNLOAD is set
 if [ -n "${CUSTOM_JARS_DOWNLOAD}" ]; then
@@ -133,13 +160,52 @@ if [ -n "${CUSTOM_JARS_DOWNLOAD}" ]; then
   done
 fi
 
-# # Use aws cli command to sync the S3 folder
-# if [ -n "${S3_URL}" ]; then
-#   mkdir ${S3_CUSTOM_JARS_DIR}
-#   echo "S3 sync from ${S3_URL}"
+# Create the appdata directory if it doesn't exist
+mkdir -p "$APPDATA_DIR"
 
-#   aws s3 sync ${S3_URL} ${S3_CUSTOM_JARS_DIR}
-# fi
+# Only attempt download if the environment variable is set
+if [ -n "$KEYSTORE_DOWNLOAD" ]; then
+    # Create the appdata directory if it doesn't exist
+    mkdir -p "$APPDATA_DIR"
+
+    # Download the keystore file quietly
+    echo "Downloading keystore from: $KEYSTORE_DOWNLOAD"
+    curl --silent --show-error -fSL "$KEYSTORE_DOWNLOAD" -o "$KEYSTORE_FILE"
+
+    # Check if the download was successful
+    if [ $? -eq 0 ]; then
+        echo "Keystore successfully downloaded to: $KEYSTORE_FILE"
+    else
+        echo "Failed to download the keystore."
+        exit 1
+    fi
+else
+    echo "KEYSTORE_DOWNLOAD is not set. Skipping keystore download."
+fi
+
+
+if ! [ -z "${KEYSTORE_TYPE+x}" ]; then
+	sed -i "s/^keystore\.type\s*=\s*.*\$/keystore.type = ${KEYSTORE_TYPE//\//\\/}/" /opt/bridgelink/conf/mirth.properties
+fi
+
+
+# database max connections
+if ! [ -z "${DATABASE_MAX_CONNECTIONS+x}" ]; then
+	sed -i "s/^database\.max-connections\s*=\s*.*\$/database.max-connections = ${DATABASE_MAX_CONNECTIONS//\//\\/}/" /opt/bridgelink/conf/mirth.properties
+fi
+
+# database max retries
+if ! [ -z "${DATABASE_MAX_RETRY+x}" ]; then
+	sed -i "s/^database\.connection\.maxretry\s*=\s*.*\$/database.connection.maxretry = ${DATABASE_MAX_RETRY//\//\\/}/" /opt/bridgelink/conf/mirth.properties
+fi
+
+# database retry wait time
+if ! [ -z "${DATABASE_RETRY_WAIT+x}" ]; then
+	sed -i "s/^database\.connection\.retrywaitinmilliseconds\s*=\s*.*\$/database.connection.retrywaitinmilliseconds = ${DATABASE_RETRY_WAIT//\//\\/}/" /opt/bridgelink/conf/mirth.properties
+fi
+
+
+
 
 cd /opt/bridgelink
 
